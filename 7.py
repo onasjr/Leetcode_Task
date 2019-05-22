166eacf6be36e3dea7e5bb8eebb1b82d089e6732
zll1114ali <50893095+zll1114ali@users.noreply.github.com>
2019-05-22 13:11:56 +0800

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,26 @@
+import pdb
+import copy
+class Solution(object):
+    # 本题的思路就是先判断给定整数x的正负情况，把符号首先给提取出来
+    # 然后将abs(x)变成字符串，接着将字符串反转，最后恢复成整数
+    def reverse(self, x):
+        """
+                :type x: int
+                :rtype: int
+        """
+        if x>=0:flag = 1
+        else:flag = -1
+        str_x = str(abs(x))
+        str_x_reverse = str_x[::-1]
+        # pdb.set_trace()
+        x_reverse = int(str_x_reverse)
+        x_reverse = x_reverse * flag
+        if -2**31 < x_reverse< 2**31-1:
+            return x_reverse
+        else:
+            return 0
+
+
+x = -2147483630
+reverse_x = Solution().reverse(x)
+print(reverse_x)
