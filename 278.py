2ba97900b11532db52287139047cab1d1212b029
zll1114ali <50893095+zll1114ali@users.noreply.github.com>
2019-05-22 15:44:43 +0800

    Create 278.py

---
+++ 278.py
@@ -0,0 +1,22 @@
+## 查找第一个错误的版本：
+## 思路： 二分法查找
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
