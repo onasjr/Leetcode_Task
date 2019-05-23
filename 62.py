bb652a8594015716cb1e59e06086259fe4e8517f
zll1114ali <50893095+zll1114ali@users.noreply.github.com>
2019-05-23 13:25:38 +0800

    Update 62.py

--- 62.py
+++ 62.py
@@ -11,3 +11,24 @@ class Solution(object):
         ans = math.factorial(m+n-2)/math.factorial(n-1)/math.factorial(m-1)
 
         return ans
+
+## 动态规划
+
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m < 1 or n < 1:
+            return 0
+        dp = [[0]*n]*m
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
