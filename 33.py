73f02b94884cbceba9dfdf397d6e40067402c161
zll1114ali <50893095+zll1114ali@users.noreply.github.com>
2019-05-25 14:42:49 +0800

    Create 33.py

---
+++ 33.py
@@ -0,0 +1,14 @@
+## 搜索旋转排序数组
+## 库函数
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
