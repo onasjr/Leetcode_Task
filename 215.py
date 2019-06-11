31944b835df0ef350bfc42710474fcdf26c27175
zll1114ali <50893095+zll1114ali@users.noreply.github.com>
2019-06-11 09:58:13 +0800

    Create 215.py

---
+++ 215.py
@@ -0,0 +1,13 @@
+## 数组中的第K个最大元素
+## 排序，取-k位
+
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        return nums[-k]
