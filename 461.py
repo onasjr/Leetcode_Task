4111eb8395bab86e4ca8d45a3f667df1aa8f9bbb
zll1114ali <50893095+zll1114ali@users.noreply.github.com>
2019-06-14 15:17:34 +0800

    Create 461.py

---
+++ 461.py
@@ -0,0 +1,10 @@
+## 计算异或中1的个数
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x ^ y).count("1")
